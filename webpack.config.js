d99fc555afdf5dabed2b0e6b59488c9b80da52d9
Camilo318 <camiloa0318@gmail.com>
2020-08-23 13:41:03 -0500

    Using HashRouter instead of BrowserRouter, publicPath updated

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/'
+        publicPath: './'
     },
     resolve: {
         extensions: ['.jsx', '.js']
@@ -39,7 +39,7 @@ module.exports = {
                 loader: 'file-loader',
                 options: {
                     name: '[name][hash].[ext]',
-                    outputPath: 'assets'
+                    outputPath: 'assets/images'
                 }
             }
         ]
